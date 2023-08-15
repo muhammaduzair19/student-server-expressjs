a6005b9d5eb6aa7e5fe7107342dfcee14e084fbe
muhammaduzair19 <muhammaduzairilyas@gmail.com>
2023-08-15 14:36:49 +0500

    get only one student api

--- server.js
+++ server.js
@@ -9,9 +9,25 @@ app.use(cors());
 const studentData = fs.readFileSync("./data.json", "utf-8");
 const parsedData = JSON.parse(studentData);
 
+app.use(express.json())
+
 app.get("/v1/studentData/getStudent", (req, res) => {
   res.status(200).json({
-    data: parsedData,
+    data: parsedData
+  });
+});
+
+app.get("/v1/studentData/getStudent/:rollNo", (req, res) => {
+
+  if (+(req.params.rollNo) > parsedData?.data.length) {
+    return res.status(400).send("Data Not Available")
+  }
+
+  const singleStudent = parsedData?.data?.find(student => student.rollNo == req.params.rollNo)
+  console.log(singleStudent)
+
+  res.status(200).json({
+    data: singleStudent
   });
 });
 
